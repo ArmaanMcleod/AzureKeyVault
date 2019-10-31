1d0fbcc1a33aed552a510f19e17ccbd54e43d254
OpticGenius <opticgenius@hotmail.com>
2019-10-31 12:49:30 +1100

    Added working version of EnableKeyVaultRegeration

--- scripts/EnableKeyVaultRegeneration.ps1
+++ scripts/EnableKeyVaultRegeneration.ps1
@@ -14,26 +14,41 @@ param (
     $ResourceGroupName,
     # Regeneration period in datys
     [Parameter(Mandatory = $true)]
-    [int]
+    [double]
     $RegenerationPeriod
 )
 
-# Get storage account
+# Public used key vault
+$keyVaultSpAppId = "cfa8b339-82a2-471a-a3c9-0fc0be7a4093"
+
+# Storage access key name
+$storageAccountKey = "key1"
+
+# Get your User Id
+$userId = (Get-AzContext).Account.Id
+
+# Get a reference to your Azure storage account
 $storageAccount = Get-AzStorageAccount `
     -ResourceGroupName $ResourceGroupName `
-    -Name $StorageAccountName
+    -StorageAccountName $StorageAccountName
 
-# Get storage account keys
-$storageAccountKey = Get-AzStorageAccountKey `
-    -ResourceGroupName $ResourceGroupName `
-    -Name $StorageAccountName
+# Assign RBAC role "Storage Account Key Operator Service Role" to Key Vault, limiting the access scope to your storage account. For a classic storage account, use "Classic Storage Account Key Operator Service Role." 
+New-AzRoleAssignment `
+    -ApplicationId $KeyVaultSpAppId `
+    -RoleDefinitionName 'Storage Account Key Operator Service Role' `
+    -Scope $StorageAccount.Id
 
+# Give your user principal access to all storage account permissions, on your Key Vault instance
+Set-AzKeyVaultAccessPolicy `
+    -VaultName $KeyVaultName `
+    -UserPrincipalName $userId `
+    -PermissionsToStorage get, list, delete, set, update, regeneratekey, getsas, listsas, deletesas, setsas, recover, backup, restore, purge
+
+# Add your storage account to your Key Vault's managed storage accounts
 $regenPeriod = [System.Timespan]::FromDays($RegenerationPeriod)
-
-# Add regeneration period of key
 Add-AzKeyVaultManagedStorageAccount `
     -VaultName $KeyVaultName `
     -AccountName $StorageAccountName `
-    -AccountResourceId $storageAccount.Id `
-    -ActiveKeyName $storageAccountKey.KeyName[0] `
+    -AccountResourceId $StorageAccount.Id `
+    -ActiveKeyName $StorageAccountKey `
     -RegenerationPeriod $regenPeriod
